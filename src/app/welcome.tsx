29f9f37958fe586926e5c00787a986756e576e9c
neel-asanulhaquekiron <asanulhaquekiron@gmail.com>
2026-07-18 02:15:17 -0500

    add Welcome component with ScreenWrapper for layout

---
+++ src/app/welcome.tsx
@@ -0,0 +1,14 @@
+import ScreenWrapper from "@/components/ScreenWrapper";
+import { StyleSheet, Text } from "react-native";
+
+const Welcome = () => {
+  return (
+    <ScreenWrapper>
+      <Text>welcome</Text>
+    </ScreenWrapper>
+  );
+};
+
+export default Welcome;
+
+const styles = StyleSheet.create({});
