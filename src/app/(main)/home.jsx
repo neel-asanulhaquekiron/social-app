b3e59c9813e71bc4f8618cc54a2038098e5e667b
neel-asanulhaquekiron <asanulhaquekiron@gmail.com>
2026-07-18 09:52:01 -0500

    feat: implement Home component layout with header and navigation icons

--- src/app/(main)/home.jsx
+++ src/app/(main)/home.jsx
@@ -1,6 +1,9 @@
 import ScreenWrapper from "@/components/ScreenWrapper";
+import { theme } from "@/constants/theme";
+import { hp, wp } from "@/helpers/common";
 import { supabase } from "@/lib/supabase";
-import { Alert, Button, StyleSheet, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Alert, Pressable, StyleSheet, Text, View } from "react-native";
 
 const Home = () => {
   const onLogout = async () => {
@@ -11,13 +14,63 @@ const Home = () => {
   };
 
   return (
-    <ScreenWrapper>
-      <Text>Home</Text>
-      <Button title="logout" onPress={onLogout} />
+    <ScreenWrapper bg="white">
+      <View style={styles.container}>
+        {/* header */}
+        <View style={styles.header}>
+          <Text style={styles.title}>Social App</Text>
+          <View style={styles.icons}>
+            <Pressable onPress={() => router.push("notifications")}>
+              <Ionicons
+                name="notifications-outline"
+                size={hp(3.2)}
+                color={theme.colors.text}
+              />
+            </Pressable>
+
+            <Pressable onPress={() => router.push("newPost")}>
+              <Ionicons
+                name="add-circle-outline"
+                size={hp(3.2)}
+                color={theme.colors.text}
+              />
+            </Pressable>
+
+            <Pressable onPress={() => router.push("profile")}>
+              <Ionicons
+                name="person-circle-outline"
+                size={hp(3.2)}
+                color={theme.colors.text}
+              />
+            </Pressable>
+          </View>
+        </View>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: wp(4),
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: hp(3.2),
+    fontWeight: theme.fonts.bold,
+    color: theme.colors.text,
+  },
+  icons: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 18,
+  },
+});
