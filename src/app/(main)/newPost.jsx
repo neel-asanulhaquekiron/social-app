4e3c95c8ba87b30dff65fb49ac1a1688ee8535ad
neel-asanulhaquekiron <asanulhaquekiron@gmail.com>
2026-07-18 09:55:43 -0500

    feat: create NewPost, Notifications, and Profile components with basic layout

---
+++ src/app/(main)/newPost.jsx
@@ -0,0 +1,14 @@
+import ScreenWrapper from "@/components/ScreenWrapper";
+import { StyleSheet, Text } from "react-native";
+
+const NewPost = () => {
+  return (
+    <ScreenWrapper bg="white">
+      <Text>NewPost</Text>
+    </ScreenWrapper>
+  );
+};
+
+export default NewPost;
+
+const styles = StyleSheet.create({});
