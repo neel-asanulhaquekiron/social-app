3c0a608eb21326648996fbfb7853084079396e44
neel-asanulhaquekiron <asanulhaquekiron@gmail.com>
2026-07-18 03:00:12 -0500

    add Button component with loading state and shadow styling

---
+++ src/components/Button.jsx
@@ -0,0 +1,59 @@
+import { theme } from "@/constants/theme";
+import { hp } from "@/helpers/common";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import Loading from "./Loading";
+
+const Button = ({
+  buttonStyle,
+  textStyle,
+  title = "Button",
+  onPress = () => {},
+  loading = false,
+  hasShadow = true,
+}) => {
+  const shadowStyle = {
+    shadowColor: theme.colors.dark,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  };
+
+  if (loading) {
+    return (
+      <View style={[styles.button, buttonStyle, { backgroundColor: "white" }]}>
+        <Loading />
+      </View>
+    );
+  }
+
+  return (
+    <Pressable
+      style={[styles.button, buttonStyle, hasShadow && shadowStyle]}
+      onPress={onPress}
+    >
+      <Text style={[styles.text, textStyle]}>{title}</Text>
+    </Pressable>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: theme.colors.primary,
+    height: hp(6.6),
+    justifyContent: "center",
+    alignItems: "center",
+    borderCurve: "continuous",
+    borderRadius: theme.radius.xl,
+  },
+  text: {
+    fontSize: hp(2.5),
+    color: "white",
+    fontWeight: theme.fonts.bold,
+  },
+});
