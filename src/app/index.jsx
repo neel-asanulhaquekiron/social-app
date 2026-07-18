0ce42f41cfe7fcc1b5227058b5e76aed874b4f17
neel-asanulhaquekiron <asanulhaquekiron@gmail.com>
2026-07-18 02:15:20 -0500

    add index component with navigation to Welcome screen

---
+++ src/app/index.jsx
@@ -0,0 +1,15 @@
+import ScreenWrapper from "@/components/ScreenWrapper";
+import { useRouter } from "expo-router";
+import { Button, Text } from "react-native";
+
+const index = () => {
+  const router = useRouter();
+  return (
+    <ScreenWrapper>
+      <Text>index</Text>
+      <Button title="Go to Welcome" onPress={() => router.push("welcome")} />
+    </ScreenWrapper>
+  );
+};
+
+export default index;
