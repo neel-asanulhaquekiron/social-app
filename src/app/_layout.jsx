ab55e0f58af1e4d97aebf3c37f9056ead6c6237a
neel-asanulhaquekiron <asanulhaquekiron@gmail.com>
2026-07-18 09:46:59 -0500

    feat: enhance user authentication flow by fetching user data on session change

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -2,6 +2,7 @@ import { AuthProvider, useAuth } from "@/context/AuthContext";
 import { supabase } from "@/lib/supabase";
 import { Stack, useRouter } from "expo-router";
 import { useEffect } from "react";
+import { getUserData } from "../../services/userService";
 
 const _layout = () => {
   return (
@@ -12,14 +13,21 @@ const _layout = () => {
 };
 
 const MainLayout = () => {
-  const { setAuth } = useAuth();
+  const { setAuth, setUserData } = useAuth();
   const router = useRouter();
+
+  const updateUserData = async (user) => {
+    const res = await getUserData(user?.id);
+    if (res.success) {
+      setUserData(res.data);
+    }
+  };
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((_event, session) => {
-      console.log("session user", session?.user?.id);
       if (session) {
         setAuth(session.user);
+        updateUserData(session.user);
         router.push("/home");
       } else {
         setAuth(null);
