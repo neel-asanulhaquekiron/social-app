a3c076e952e74688be4e45a650cbc326c6a0d90c
neel-asanulhaquekiron <asanulhaquekiron@gmail.com>
2026-07-19 04:46:52 -0500

    feat: implement notifications screen with fetching and displaying notifications

--- src/app/(main)/notifications.jsx
+++ src/app/(main)/notifications.jsx
@@ -1,14 +1,67 @@
+import Header from "@/components/Header";
+import NotificationItem from "@/components/NotificationItem";
 import ScreenWrapper from "@/components/ScreenWrapper";
-import { StyleSheet, Text } from "react-native";
+import { useAuth } from "@/context/AuthContext";
+import { wp } from "@/helpers/common";
+import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { fetchNotifications } from "../../../services/notificationServices";
 
 const Notifications = () => {
+  const [notifications, setNotifications] = useState([]);
+  const { user } = useAuth();
+  const router = useRouter();
+
+  const getNotifications = async () => {
+    try {
+      const { success, data, error } = await fetchNotifications(user?.id);
+      if (success) {
+        setNotifications(data);
+      } else {
+        console.error("Error fetching notifications:", error);
+      }
+    } catch (error) {
+      console.error("Error fetching notifications:", error);
+    }
+  };
+
+  useEffect(() => {
+    getNotifications();
+  }, []);
   return (
     <ScreenWrapper bg="white">
-      <Text>Notifications</Text>
+      <View style={styles.container}>
+        <Header title="Notifications" />
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.scrollContainer}
+        >
+          {notifications.map((notification) => {
+            return (
+              <NotificationItem
+                item={notification}
+                key={notification?.id}
+                router={router}
+              />
+            );
+          })}
+          {notifications.length === 0 && <Text>No notifications yet.</Text>}
+        </ScrollView>
+      </View>
     </ScreenWrapper>
   );
 };
-
 export default Notifications;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingHorizontal: wp(4),
+  },
+  scrollContainer: {
+    paddingVertical: 20,
+    gap: 5,
+  },
+});
