dcde272ed2a0160c3cf4a81b16e07a9f188d20d2
neel-asanulhaquekiron <asanulhaquekiron@gmail.com>
2026-07-19 04:46:22 -0500

    feat: add fetchNotifications function to retrieve notifications for a user

--- services/notificationServices.js
+++ services/notificationServices.js
@@ -1,3 +1,5 @@
+import { supabase } from "@/lib/supabase";
+
 export const createNotification = async (notificationData) => {
   try {
     const { data, error } = await supabase
@@ -14,6 +16,26 @@ export const createNotification = async (notificationData) => {
     return { success: true, data };
   } catch (error) {
     console.error("Error creating notification:", error);
+    return { success: false, error };
+  }
+};
+
+export const fetchNotifications = async (receiverId) => {
+  try {
+    const { data, error } = await supabase
+      .from("notifications")
+      .select("*, sender: senderId (id, name)")
+      .eq("receiverId", receiverId)
+      .order("created_at", { ascending: false });
+
+    if (error) {
+      console.error("Error fetching notifications:", error);
+      return { success: false, error };
+    }
+
+    return { success: true, data };
+  } catch (error) {
+    console.error("Error fetching notifications:", error);
     return { success: false, error };
   }
 };
