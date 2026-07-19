7714fff8cc74dd03ea421f72f84bf20d76cb700e
neel-asanulhaquekiron <asanulhaquekiron@gmail.com>
2026-07-19 12:54:14 -0500

    fix(db): update Supabase client initialization to use service role key

--- server/config/db.js
+++ server/config/db.js
@@ -2,14 +2,8 @@ const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-});
+const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
 
 module.exports = supabase;
