c2b35c7279e8b15599378e19db850f2bbe653af7
neel-asanulhaquekiron <asanulhaquekiron@gmail.com>
2026-07-19 09:13:42 -0500

    feat: integrate Supabase for post management and set up Express server
    - Added Supabase client configuration in scripts/config/db.js
    - Implemented Post model with methods for creating/updating and fetching posts in scripts/models/post.js
    - Created Express server in server.js to handle post-related routes
    - Updated package.json to include necessary dependencies and types

---
+++ models/post.js
@@ -0,0 +1,68 @@
+const supabase = require("../server/config/db");
+
+class Post {
+  static async createOrUpdatePost(postData) {
+    try {
+      const { data, error } = await supabase
+        .from("posts")
+        .upsert(post)
+        .select()
+        .single();
+
+      if (error) {
+        console.error("Error creating/updating post:", error);
+        return { success: false, msg: error.message };
+      }
+
+      return { success: true, data };
+    } catch (error) {
+      console.error("Error creating/updating post:", error);
+      return { success: false, msg: error.message || "Something went wrong" };
+    }
+  }
+
+  static async fetchPosts(limit = 10) {
+    try {
+      const { data, error } = await supabase
+        .from("posts")
+        .select("*, user: users (id, name), postLikes (*), comments (count)")
+        .order("created_at", { ascending: false })
+        .limit(limit);
+
+      if (error) {
+        console.error("Error fetching posts:", error);
+        return { success: false, msg: error.message };
+      }
+
+      return { success: true, data };
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+      return { success: false, msg: error.message || "Something went wrong" };
+    }
+  }
+
+  static async fetchPostById(postId) {
+    try {
+      const { data, error } = await supabase
+        .from("posts")
+        .select(
+          "*, user: users (id, name), postLikes (*), comments (* , user: users(id, name))",
+        )
+        .eq("id", postId)
+        .order("created_at", { ascending: false, foreignTable: "comments" })
+        .single();
+
+      if (error) {
+        console.error("Error fetching post by ID:", error);
+        return { success: false, msg: error.message };
+      }
+
+      return { success: true, data };
+    } catch (error) {
+      console.error("Error fetching post by ID:", error);
+      return { success: false, msg: error.message || "Something went wrong" };
+    }
+  }
+}
+
+module.exports = Post;
