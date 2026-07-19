0b2d9cedcbcbed51549ab3d410d6f1c7ad4e3278
neel-asanulhaquekiron <asanulhaquekiron@gmail.com>
2026-07-19 12:54:53 -0500

    feat: add user, notification, and auth routers to server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const postRouter = require("./router/postRouter.js");
+const userRouter = require("./router/userRouter.js");
+const notificationRouter = require("./router/notificationRouter.js");
+const authRouter = require("./router/authRouter.js");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -7,6 +10,9 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 
 app.use("/posts", postRouter);
+app.use("/users", userRouter);
+app.use("/notifications", notificationRouter);
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
