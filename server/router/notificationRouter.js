4e54860d32acda264ab624d839306617ebc6cc30
neel-asanulhaquekiron <asanulhaquekiron@gmail.com>
2026-07-19 12:54:45 -0500

    feat: add authentication, notification, and user routers with CRUD operations

---
+++ server/router/notificationRouter.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const Notification = require("../models/notification.js");
+
+router.post("/", async (req, res) => {
+  const notificationData = req.body;
+  const result = await Notification.createNotification(notificationData);
+  res.json(result);
+});
+
+router.get("/:receiverId", async (req, res) => {
+  const receiverId = req.params.receiverId;
+  const result = await Notification.fetchNotifications(receiverId);
+  res.json(result);
+});
+
+module.exports = router;
---
+++ server/router/userRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+
+router.get("/:userId", async (req, res) => {
+  const userId = req.params.userId;
+  const result = await User.getUserData(userId);
+  console.log("🚀 ~ result:", result);
+  res.json(result);
+});
+
+module.exports = router;
