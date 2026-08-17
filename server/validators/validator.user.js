a64279781dfaafdcfdae7150896e6a89714c169e
neel-asanulhaquekiron <asanulhaquekiron@gmail.com>
2026-08-17 07:25:08 -0500

    refactor: add schema for validating push token registration

--- server/validators/validator.user.js
+++ server/validators/validator.user.js
@@ -4,6 +4,11 @@ class UserValidator {
   static userIdParamsSchema = z.object({
     userId: z.string().uuid(),
   });
+
+  static registerPushTokenSchema = z.object({
+    userId: z.string().uuid(),
+    pushToken: z.string().min(1),
+  });
 }
 
 module.exports = UserValidator;
